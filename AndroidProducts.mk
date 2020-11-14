d60d498404b15f75ff7f1911ff8e290580f5709a
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-11-14 17:06:45 +0530

    avicii: Superior Bringup
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ifd1393fe46b5cbf1f23ea6eca49df8df0551d715

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixys_avicii.mk
+    $(LOCAL_DIR)/superior_avicii.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_avicii-userdebug \
-    pixys_avicii-eng
+    superior_avicii-userdebug \
+    superior_avicii-eng
--- superior_avicii.mk
+++ superior_avicii.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # PixysOS Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := pixys_avicii
+PRODUCT_NAME := superior_avicii
 PRODUCT_DEVICE := avicii
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
