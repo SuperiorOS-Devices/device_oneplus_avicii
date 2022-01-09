cd186e75974b03bc1f136a82adf40da8cedad987
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2022-01-09 19:31:56 +0530

    avicii: SuperiorOS bringup
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: If2cc31dd94182de5e910e52d8c6d1b04a8626014

--- superior_avicii.mk
+++ superior_avicii.mk
@@ -21,14 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from avicii device
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+#Gapps
+TARGET_GAPPS_ARCH := arm64
 
 # PixelExperience Properties
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_USES_CUSTOM_AVB_KEY := true
 
-PRODUCT_NAME := aosp_avicii
+PRODUCT_NAME := superior_avicii
 PRODUCT_DEVICE := avicii
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
