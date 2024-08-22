sreeshankark/android_device_oneplus_avicii-infinityx

[SQUASH] avicii: Merge `hardware/oplus` into device tree
commit 733fbeccbee412cd94e94cf8510da2564474d628
Author: LuK1337 <priv.luk@gmail.com>
Date:   Sat May 18 20:59:33 2024 +0200

    Remove libqti-perfd-client

    Accidentally left in HEAD~.

    Change-Id: I419662af0a6eeb8978270b750406bf9f5e99727a

commit 77fcc092b54de6a7aa68545d3ba4924ecc0c1fd4
Author: Michael Bestas <mkbestas@lineageos.org>
Date:   Sat May 18 09:14:16 2024 +0300

    Move to common power-libperfmgr

    Change-Id: Icaa3b265fbc0fb71ae57a5cbde1654f62d07aef8

commit 1e8889a26be733d3eb365a1bea68894655028aad
Author: LineageOS Infra <infra@lineageos.org>
Date:   Wed May 15 15:50:24 2024 +0000

    Automatic translation import

    Change-Id: Id3e4f6639374151aeabcda0f41810195e0b4aa8f

commit 67fda57ebc78e1477e212997396514ee2cfab3d9
Author: inferno0230 <mail@inferno0230.in>
Date:   Fri Feb 16 12:38:24 2024 +0530

    sepolicy: qti: Initial oplus touch/daemon rules

    Change-Id: Ic98e14cd7d6b33085ff3dc85331b78c905853069

commit 7d98a588a43259a0b009b3d2bda02ed49676c116
Author: Venkat Yadlapati <venkat.yadlapati@hotmail.com>
Date:   Tue Feb 27 23:25:00 2024 +0530

    sepolicy: qti: Label more SM7550 wakeup nodes

    Change-Id: Iab5d5bdb3339a6e06dfc368c21d902065a8acd85

commit 5a7aae02654005eff1f8fe36cab0b84896d260a2
Author: inferno0230 <mail@inferno0230.in>
Date:   Mon May 6 18:41:05 2024 +0530

    sepolicy: qti: Label SM7550 wakeup nodes

    Change-Id: Ic0ff77c875a6e4eaf898473d085b839061d8380b

commit 38a55278cd2b7dd3a854c161b02f9c893a5eb54c
Author: inferno0230 <mail@inferno0230.in>
Date:   Mon May 6 18:38:07 2024 +0530

    sepolicy: qti: Label additional SM8550 wakeup nodes

    Change-Id: I4a892bf559fc9e8e0202782f3019484221cecd57

commit 6a5a1daec329f9380d0702efc257532d8258d528
Author: inferno0230 <mail@inferno0230.in>
Date:   Mon May 6 09:33:33 2024 +0530

    sepolicy: qti: Label oplus_mms node

    Change-Id: If11603521c53c1a3810d6e3cb8762c1b61944eec

commit 4b9621b59b0c12c15338e78f6082eaf5

sepolicy/qti/SEPolicy.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/vendor \
+    device/oneplus/avicii/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/private \
+    device/oneplus/avicii/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/public \
+    device/oneplus/avicii/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+
+ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/vendor/common-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/private/common-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/public/common-um
+endif
+
+include device/infinity/sepolicy/libperfmgr/sepolicy.mk

SuperiorOS-Devices/device_oneplus_avicii

sepolicy/qti/SEPolicy.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/vendor \
+    device/oneplus/avicii/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/private \
+    device/oneplus/avicii/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/public \
+    device/oneplus/avicii/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+
+ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/vendor/common-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/private/common-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/oneplus/avicii/sepolicy/qti/public/common-um
+endif
+
+include device/superior/sepolicy/libperfmgr/sepolicy.mk